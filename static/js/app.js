fa57bbba3910e1c3e538cebbe2765265d9bbfc49
Bradley Medford <bradleymedford@Bradleys-MacBook-Pro.local>
2021-04-05 14:53:00 -0400

    updated app.js with code to pull data to website.

--- static/js/app.js
+++ static/js/app.js
@@ -2,3 +2,16 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+// Appending the table with the Sightings data
+var tbody = d3.select("tbody");
+
+tableData.forEach(function(sighting) {
+    console.log(sighting);
+    var row = tbody.append("tr");
+
+    Object.entries(sighting).forEach(function([key, value]) {
+        console.log(key, value);
+        var cell = row.append("td");
+        cell.text(value);
+    });
+})
